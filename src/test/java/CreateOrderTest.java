f2ee2ace9c206a5ffee0ab756c3712aba609da68
irasurckova2013 <ira.surckova2013@yandex.ru>
2025-01-14 09:27:17 +0400

    Sprint_7

---
+++ src/test/java/CreateOrderTest.java
@@ -0,0 +1,36 @@
+import io.restassured.response.Response;
+import org.example.Orders;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@RunWith(Parameterized.class)
+public class CreateOrderTest {
+
+    private Orders order;
+    private CourierSteps courierSteps = new CourierSteps();
+
+    public CreateOrderTest(Orders order) {
+        this.order = order;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][] {
+                {OrdersTestData.bodyPostWithBlack()},
+                {OrdersTestData.bodyPostWithGrey()},
+                {OrdersTestData.bodyPostWithBothColors()},
+                {OrdersTestData.bodyPostWithNonColors()}
+        };
+    }
+
+    @Test
+    public void createOrder() {
+        Response createOrder = courierSteps.createOrder(order);
+        createOrder.then().body("track", notNullValue());
+        createOrder.then().statusCode(201);
+    }
+
+}
